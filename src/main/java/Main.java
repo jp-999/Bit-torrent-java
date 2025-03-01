9c30c29206e58578136bd78e467852949f70cbad
jp-999 <jayanthpeddi999@gmail.com>
2025-03-01 22:31:33 +0530

    stage 5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 import com.google.gson.Gson;
 // import com.dampcake.bencode.Bencode; - available if you need it!
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
   private static final Gson gson = new Gson();
@@ -37,11 +39,30 @@ public class Main {
         }
       }
       int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
-      return "\"" + bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length) + "\"";
+      return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length);
     } else if (bencodedString.startsWith("i")) {
       return Long.parseLong(bencodedString.substring(1, bencodedString.indexOf("e")));
+    } else if (bencodedString.startsWith("l")) {
+      List<Object> list = new ArrayList<>();
+      int index = 1; // Start after 'l'
+      // Check for empty list case
+      if (bencodedString.charAt(index) == 'e') {
+        return list; // Return empty list
+      }
+      while (bencodedString.charAt(index) != 'e') {
+        Object element = decodeBencode(bencodedString.substring(index));
+        list.add(element);
+        // Update index to the end of the decoded element
+        if (element instanceof String) {
+          int length = ((String) element).length();
+          index += length + 2; // Length + ':' + 'e'
+        } else if (element instanceof Long) {
+          index = bencodedString.indexOf("e", index) + 1; // Move past 'e'
+        }
+      }
+      return list; // Return the decoded list
     } else {
-      throw new RuntimeException("Only strings are supported at the moment");
+      throw new RuntimeException("Only strings and lists are supported at the moment");
     }
   }
   
