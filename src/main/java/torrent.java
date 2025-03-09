a41c219465c636919af00547e7c52f395446ce7c
jp-999 <jayanthpeddi999@gmail.com>
2025-03-09 22:09:57 +0530

    stage 8

--- src/main/java/torrent.java
+++ src/main/java/torrent.java
@@ -3,20 +3,23 @@ import com.dampcake.bencode.Type;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Map;
+
 public class Torrent {
-  public String announce;
-  public long length;
-  public byte[] infoHash;
-  public Torrent(byte[] bytes) throws NoSuchAlgorithmException {
-    Bencode bencode = new Bencode(false);
-    Bencode bencode2 = new Bencode(true);
-    Map<String, Object> root = bencode.decode(bytes, Type.DICTIONARY);
-    Map<String, Object> info = (Map<String, Object>)root.get("info");
-    announce = (String)root.get("announce");
-    length = (long)info.get("length");
-    MessageDigest digest2 = MessageDigest.getInstance("SHA-1");
-    infoHash = digest2.digest(bencode2.encode(
-        (Map<String, Object>)bencode2.decode(bytes, Type.DICTIONARY)
-            .get("info")));
-  }
+    public String announce;
+    public long length;
+    public byte[] infoHash;
+
+    public Torrent(byte[] bytes) throws NoSuchAlgorithmException {
+        Bencode bencode = new Bencode(true);
+        
+        Map<String, Object> root = bencode.decode(bytes, Type.DICTIONARY);
+        Map<String, Object> info = (Map<String, Object>) root.get("info");
+        
+        announce = (String) root.get("announce");
+        length = (long) info.get("length");
+        
+        byte[] encodedInfo = bencode.encode(info);
+        MessageDigest digest = MessageDigest.getInstance("SHA-1");
+        infoHash = digest.digest(encodedInfo);
+    }
 }
